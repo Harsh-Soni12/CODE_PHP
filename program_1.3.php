60f6119d4deffbce5e4a3de23bc8dcd1b5e92fb1
Harsh-Soni12 <harsh.vaya127933@marwadiuniversity.ac.in>
2026-06-13 10:16:21 +0530

    This is program1.3

---
+++ program_1.3.php
@@ -0,0 +1,14 @@
+<?php
+
+// 1.3 Write a PHP program for operators in PHP.
+
+$a = 10;
+$b = 5;
+
+echo "Addition = ".($a+$b)."<br>
+Subtraction = ".($a-$b)."<br>
+Multiplication = ".($a*$b)."<br>
+Division = ".($a/$b)."<br>
+Modulus = ".($a%$b);
+
+?>
