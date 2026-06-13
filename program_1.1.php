f16a4ecbc22a6ad0e2e422557c83a395fb328076
Harsh-Soni12 <harsh.vaya127933@marwadiuniversity.ac.in>
2026-06-13 10:13:37 +0530

    This is program1.1

---
+++ program_1.1.php
@@ -0,0 +1,19 @@
+<?php
+// 1.1 Write a PHP program for print Previous semester Result using variables & constants in PHP.
+echo "<h2>Collage Result<br></h2> ";
+echo "================================================= <br> " ;
+echo "Student Name : Mitesh<br> ";
+echo "Course :BCA <br> ";
+echo " Enrollment no : 9234 <br>";
+echo  "  Class : C <br> ";
+echo " ================================================= <br> ";
+echo "python :- A+  <br> ";
+echo "Java :- A+ <br> ";
+echo "c# :- A <br> ";
+echo "os :- A+ <br> ";
+echo "cn :- A+ <br> ";
+echo "=================================================== <br> ";
+echo " sgpa : 8.6 <br> ";
+echo " cgpa : 8.8  <br> ";
+echo "===================================================== <br> ";
+?>
