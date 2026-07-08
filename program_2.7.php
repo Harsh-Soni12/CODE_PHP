481d00d499dc2246b21a3f34d9688ab6ae6d0464
Harsh-Soni12 <harsh.vaya127933@marwadiuniversity.ac.in>
2026-07-08 22:04:32 +0530

    This is program2.7

---
+++ program_2.7.php
@@ -0,0 +1,10 @@
+<?php
+date_default_timezone_set("Asia/Kolkata");
+
+echo "Current India Time: " . date("H:i:s") . "<br><br>";
+
+echo "1. HOUR(): " . date("H") . "<br>";
+echo "2. MINUTE(): " . date("i") . "<br>";
+echo "3. SECOND(): " . date("s") . "<br>";
+echo "4. DATE_FORMAT(): " . date("d-m-Y H:i:s") . "<br>";
+?>
