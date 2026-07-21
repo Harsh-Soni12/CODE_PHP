d34872ddd9a9799597fc4fbbe97332b41c49befb
Harsh-Soni12 <harsh.vaya127933@marwadiuniversity.ac.in>
2026-07-21 22:48:05 +0530

    This is student table

---
+++ student_tab.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Detils</title>
+</head>
+<body>
+
+<h2>Srudent Info</h2>
+
+<form method="post">
+
+Name :
+<input type="text" name="name" required>
+<br><br>
+
+Password :
+<input type="password" name="password" required>
+<br><br>
+
+Confirm Password :
+<input type="password" name="confirm" required>
+<br><br>
+
+GRNO:
+<input type="GRNO" name="GRNO" required>
+<br><br>
+
+Email :
+<input type="email" name="email" required>
+<br><br>
+
+Phone :
+<input type="text" name="phone"  required>
+<br><br>
+
+<input type="submit" name="signup" value="Sign Up">
+
+</form>
+
+</body>
+</html>
+
+<?php
+
+$conn = mysqli_connect("localhost","root","sgmh3797@dx","user");
+
+if(!$conn)
+{
+    die("Connection Failed : " . mysqli_connect_error());
+}
+
+if(isset($_POST['signup']))
+{
+    $name = $_POST['name'];
+    $password = $_POST['password'];
+    $confirm = $_POST['confirm'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+
+    if($password != $confirm)
+    {
+        echo "Password Does Not Match";
+    }
+    else
+    {
+        $sql = "INSERT INTO sign_up(name,password,email,phone)
+                VALUES('$name','$password','$email','$phone')";
+
+        if(mysqli_query($conn,$sql))
+        {
+            echo "Registration Successful";
+        }
+        else
+        {
+            echo "Error : " . mysqli_error($conn);
+        }
+    }
+}
+
+mysqli_close($conn);
+
+?>
